b361f6be1f9ce41242f237c9b998673656c3a2b9
Moises Sacal <moisbo@gmail.com>
2021-03-17 21:38:00 +1100

    changed class

--- test/aws-cdk-sftp-efs.test.ts
+++ test/aws-cdk-sftp-efs.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as AwsCdkSftpS3 from '../lib/aws-cdk-sftp-s3-stack';
+import * as AwsCdkSftpS3 from '../lib/aws-cdk-sftp-efs-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
